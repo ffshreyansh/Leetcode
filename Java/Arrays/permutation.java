7337b345521970bbd6c66ea92e1482ee3926fa16
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-08 23:41:09 +0530

    leetcode solutions

---
+++ Java/Arrays/permutation.java
@@ -0,0 +1,11 @@
+class permutation {
+    public int[] buildArray(int[] nums) {
+
+        int ans[] = new int[nums.length];
+        for (int i = 0; i < nums.length-1; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return nums;
+        
+    }
+}
